6f7c9a7837e96b6e843218087dff0ffc7ff11555
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-03-28 20:50:39 +0200

    feat: call highlight.js during compile time

--- .eleventy.js
+++ .eleventy.js
@@ -32,6 +32,22 @@ module.exports = function (eleventyConfig) {
     }
   });
 
+  eleventyConfig.addTransform("highlight", async function(content, outputPath) {
+    const hljs = require('highlight.js');
+    const JSSoup = require('jssoup').default;
+
+    if (outputPath.endsWith(".html")) {
+      const soup = new JSSoup(content);
+
+      soup.findAll('code').forEach(code => code.replaceWith("<code>" + hljs.highlight(code.text, {language: 'typescript'}).value + "</code>"))
+
+      return soup.prettify();
+    }
+    
+
+    return content; 
+  });
+
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (outputPath.endsWith(".html")) {
       let minified = htmlmin.minify(content, {
